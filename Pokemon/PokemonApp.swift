5aa578b9f54f39721179dfeee12d62d487640325
Lucas-Mochon <mochonlucas@gmail.com>
2026-02-03 13:26:33 +0100

    Initial Commit

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Cours on 03/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonTests/PokemonTests.swift
@@ -0,0 +1,16 @@
+//
+//  PokemonTests.swift
+//  PokemonTests
+//
+//  Created by Cours on 03/02/2026.
+//
+
+import Testing
+
+struct PokemonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
